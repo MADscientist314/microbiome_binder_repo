7b29bc67ba20d77869e3e42117f9a8cb918ed48d
MADscientist314 <algaenic@gmail.com>
2020-07-15 12:45:09 -0500

    update binder

--- 07152020_commands.R
+++ 07152020_commands.R
@@ -2,7 +2,7 @@
 #and was originally designed to be used at the following binder repo:
 #https://mybinder.org/v2/gh/MADscientist314/microbiome_binder_repo/master
 
-
+setwd("D:/github/microbiome_binder_repo")
 
 #Import the libraries
 library(phyloseq)
@@ -17,6 +17,19 @@ library(metacoder)
 library(knitr)
 library(tibble)
 
+
+# Note that this particular approach will be super slow.
+# And take just as long everytime you edit your code
+library(holepunch)
+write_install() # Writes install.R with all your dependencies
+write_runtime() # Writes the date your code was last modified. Can be overridden.
+generate_badge() # Generates a badge you can add to your README. Clicking badge will launch the Binder.
+# ----------------------------------------------
+# At this time ???? push the code to GitHub ????
+# ----------------------------------------------
+# Then click the badge on your README or run
+build_binder() # to kick off the build process
+# ????????
 data(atlas1006) 
 print(atlas1006)
 
